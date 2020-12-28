6e8fefe2b70b962afaad51b8b14924ef66c685d6
marktan2017 <mark.tan.2017@sis.smu.edu.sg>
2020-12-29 03:49:12 +0800

    add custom error class to validatorwrapper

--- server/src/middleware/errorHandler.ts
+++ server/src/middleware/errorHandler.ts
@@ -1,18 +1,18 @@
 import { logger } from "../utils/loggerWrapper";
 import express, { Request, Response, NextFunction } from "express";
-import { validatorWrapperError, validatorWrapperErrorType } from "../utils/validatorWrapper";
+import { validatorWrapperError } from "../utils/validatorWrapper";
 
 export default async function (
-  err: Error | validatorWrapperErrorType,
+  err: Error | typeof validatorWrapperError,
   req: Request,
   res: Response,
   next: NextFunction,
-) {
+): Promise<void> {
   logger.error(err);
 
   const isValidatorError = err instanceof validatorWrapperError;
   if (isValidatorError) {
-    res.status(400).json({ errors: (<validatorWrapperErrorType>err).errors });
+    res.status(400).json({ errors: (<validatorWrapperError>err).errors });
   }
   res.sendStatus(400);
 }
