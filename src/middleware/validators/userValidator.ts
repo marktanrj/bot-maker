4cada772a85ece397f616a3dd07b0c7b125f3d41
marktan2017 <mark.tan.2017@sis.smu.edu.sg>
2020-12-29 01:58:17 +0800

    create central errorhandler

--- src/middleware/validators/userValidator.ts
+++ src/middleware/validators/userValidator.ts
@@ -1,7 +1,28 @@
 import bodyParser from "body-parser";
 import { Request, Response, NextFunction } from "express";
+import * as yup from "yup";
 
-export const register = (req: Request, res: Response, next: NextFunction): void => {
+export const register = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   const { username, email, password } = req.body;
-  console.log(username);
+
+  const schema = yup.object().shape({
+    username: yup.string().required(),
+    email: yup.string().email().required(),
+    password: yup.string().required().min(6),
+  });
+
+  try {
+    await schema.validate(
+      {
+        username,
+        email,
+        password,
+      },
+      { abortEarly: false },
+    );
+
+    return next();
+  } catch (err) {
+    return next(err);
+  }
 };
