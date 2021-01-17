76a4fe09d06357c8487d757402e887e8f69b7dcd
marktan2017 <mark.tan.2017@sis.smu.edu.sg>
2021-01-17 22:15:37 +0800

    add delete function

--- src/components/bot/botRoute.ts
+++ src/components/bot/botRoute.ts
@@ -8,6 +8,7 @@ router.post("/create", authenticateUser, botController.create);
 router.post("/save", authenticateUser, botController.save);
 router.get("/load", authenticateUser, botController.load);
 router.post("/build", authenticateUser, botController.save, botController.build);
+router.post("/deleteBot", authenticateUser, botController.deleteBot);
 router.get("/getBotsList", authenticateUser, botController.getBotsList);
 
 export default router;
