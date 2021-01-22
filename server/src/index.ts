17f85486951169f1e5998d1d7567cedf5401bd56
root <root@ip-172-26-7-107.ap-southeast-1.compute.internal>
2021-01-22 06:16:41 +0000

    minor edit

--- server/src/index.ts
+++ server/src/index.ts
@@ -22,13 +22,15 @@ const main = async () => {
 
   const app = express();
 
-  app.use(bodyParser.json());
   app.use(cors());
-  app.use(helmet());
+  app.options('*', cors());
+  app.use(bodyParser.json());
+  //app.use(helmet());
 
   app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token");
     next();
   });
 
